e00f27c194944f5a647d3a5439005094bd4dee94
Nelson Pedrosa Pena <xeobass@gmail.com>
2021-02-09 19:59:11 +0100

    agregadas nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { InitComponent } from './init/init.component';
 import { RegGatoComponent } from './reg-gato/reg-gato.component';
+import { RegHuchaComponent } from './reg-hucha/reg-hucha.component';
+import { RegRecaudacionComponent } from './reg-recaudacion/reg-recaudacion.component';
+import { GrafHuchasComponent } from './graf-huchas/graf-huchas.component';
 
 const routes: Routes = [
   { path: 'inicio', component: MainComponent },
   { path: 'principal', component: InitComponent },
   { path: 'reg-gato', component: RegGatoComponent },
+  { path: 'reg-hucha', component: RegHuchaComponent },
+  { path: 'reg-recaudacion', component: RegRecaudacionComponent },
+  { path: 'graf-huchas', component: GrafHuchasComponent }
 ];
 
 @NgModule({
