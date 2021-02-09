05871a4300e5c0c17b85ed84c540ba750d8e3c27
Nelson Pedrosa Pena <xeobass@gmail.com>
2021-02-09 20:01:52 +0100

    eliminadas peticiones http

--- src/app/init/init.component.ts
+++ src/app/init/init.component.ts
@@ -1,36 +1,36 @@
-import { OnInit } from '@angular/core';
 import { Component,TemplateRef  } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { HttpClient } from '@angular/common/http';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router } from '@angular/router';
+import { PeticionesService } from '../../app/peticiones.service';
 
 @Component({
   selector: 'app-init',
   templateUrl: './init.component.html',
   styleUrls: ['./init.component.css']
 })
-export class InitComponent implements OnInit {
+
+export class InitComponent {
   title = 'Colonias Canguesas';
   modalRef: BsModalRef;
   dataForm:any={name:'',pass:''};
-  constructor(private modalService: BsModalService, private httpClient: HttpClient,private route: Router) {}
-  ngOnInit(): void {
-  }
+  constructor(private modalService: BsModalService,private route: Router,private dataService:PeticionesService) {}
  
+  //apertura de modals
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
   }
  
+  //realiza una query para obtención del usuario
   confirm(datosForm,template): void {
     this.modalRef.hide();
-    this.httpClient.get(`http://192.168.0.28:9876/login?usrnombre=${datosForm.name}&usrpass=${datosForm.pass}`).subscribe(resp=>{
-    if(resp==true){
-      this.route.navigate(['inicio']);
-    }else{
-      console.log("Usuario incorrecto!");
-      this.modalRef = this.modalService.show(template, {class: 'modal-lg'});
-    }
-  });
+    this.dataService.login(datosForm.name,datosForm.pass).subscribe(resp=>{
+      if(resp==true){
+        this.route.navigate(['inicio']);
+      }else{
+        console.log("Usuario incorrecto!");
+        this.modalRef = this.modalService.show(template, {class: 'modal-lg'});
+      }
+    });
   }
  
   decline(): void {
@@ -39,6 +39,7 @@ export class InitComponent implements OnInit {
 
   login():void{}
   
+  //muestra el modal con el login
   showLoginForm(template: TemplateRef<any>):void{
     this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
   }
