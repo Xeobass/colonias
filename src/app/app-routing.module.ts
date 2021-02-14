c3825f59999cd1986e57015fddc199a8ac164636
Nelson Pedrosa Pena <xeobass@gmail.com>
2021-02-14 21:01:12 +0100

    nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { RegGatoComponent } from './reg-gato/reg-gato.component';
 import { RegHuchaComponent } from './reg-hucha/reg-hucha.component';
 import { RegRecaudacionComponent } from './reg-recaudacion/reg-recaudacion.component';
 import { GrafHuchasComponent } from './graf-huchas/graf-huchas.component';
+import { NuevaColoniaComponent } from './nueva-colonia/nueva-colonia.component';
+import { ActualizaColoniaComponent } from './actualiza-colonia/actualiza-colonia.component';
+import { MuestraDatosColoniaComponent } from './muestra-datos-colonia/muestra-datos-colonia.component';
 
 const routes: Routes = [
   { path: 'inicio', component: MainComponent },
@@ -15,7 +18,10 @@ const routes: Routes = [
   { path: 'reg-gato', component: RegGatoComponent },
   { path: 'reg-hucha', component: RegHuchaComponent },
   { path: 'reg-recaudacion', component: RegRecaudacionComponent },
-  { path: 'graf-huchas', component: GrafHuchasComponent }
+  { path: 'graf-huchas', component: GrafHuchasComponent },
+  { path: 'nueva-colonia', component: NuevaColoniaComponent },
+  { path: 'actualiza-colonia', component: ActualizaColoniaComponent },
+  { path: 'muestra-colonia', component: MuestraDatosColoniaComponent },
 ];
 
 @NgModule({
